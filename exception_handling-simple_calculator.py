97dc6ee8c3a59748475d73f2c8b349760af0812e
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-06 16:35:27 +0800

    pseudocode

---
+++ exception_handling-simple_calculator.py
@@ -0,0 +1,16 @@
+# Exception Handling - Creating a simple app calculator
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# Ask user to choose and input math operation
+
+# Ask user to input two numbers
+
+# Do the calculation
+# if addition
+# if subtraction
+# if multiplication
+# if division
+# display result
+
+# ask user if they want to try again or not
+# if yes repeat
