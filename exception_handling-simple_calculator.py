2bb9c623980f8995455a6620cb59419dfbe6abff
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-06 16:58:06 +0800

    addition is working

--- exception_handling-simple_calculator.py
+++ exception_handling-simple_calculator.py
@@ -2,11 +2,15 @@
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
 # Ask user to choose and input math operation
-
+input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
 # Ask user to input two numbers
-
+first_number = float(input("Enter your first number: "))
+second_number = float(input("Enter your second number: "))
 # Do the calculation
 # if addition
+if input_operation == "Addition":
+    result_number = first_number + second_number
+    print(result_number)
 # if subtraction
 # if multiplication
 # if division
