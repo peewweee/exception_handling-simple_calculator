c2c88be6a7c0198a89deba4729ac89172c958856
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-06 18:14:13 +0800

    added extra colors

--- exception_handling-simple_calculator.py
+++ exception_handling-simple_calculator.py
@@ -1,13 +1,19 @@
 # Exception Handling - Creating a simple app calculator
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
+import pyfiglet
+print("")
+title_text = " Simple App Calculator "
+title_line = title_text.center(150, "*")
+print(title_line)
+print("")
 def calculator_app():
     try:
         # Ask user to choose and input math operation
-        input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
+        input_operation = input("\033[0;35m Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
         # Ask user to input two numbers
-        first_number = float(input("Enter your first number: "))
-        second_number = float(input("Enter your second number: "))
+        first_number = float(input("\033[0;33m Enter your first number: "))
+        second_number = float(input(" Enter your second number: "))
         # Do the calculation
         # if addition
         if input_operation == "Addition":
@@ -24,19 +30,21 @@ def calculator_app():
         else:
             raise ValueError("Invalid math operation")
         # display result
-        print(result_number)
+        print("\033[0;34m Answer:", result_number)
         # ask user if they want to try again or not
-        retry_input = input("Do you want to try again? (Yes/No): ")
+        retry_input = input("\033[0;32m Do you want to try again? (Yes/No): ")
         # if yes repeat
         if retry_input == "Yes":
             calculator_app()
+        else:
+            print("\033[0;35m Thank you for using this calculator!\N{slightly smiling face}")
 
     # catch exceptions
     except ValueError as e:
-        print(f"Error: {e}. Please try again.")
+        print(f"\033[0;34m Error: {e}. Please try again.")
         calculator_app()
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero. Please try again.")
+        print("\033[0;34m Error: Cannot divide by zero. Please try again.")
         calculator_app()
 
 calculator_app()
