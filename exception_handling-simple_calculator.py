ea4f5bbe9811c1886b86cc76857242e1cfb96608
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-06 17:43:30 +0800

    ValueError exception is working

--- exception_handling-simple_calculator.py
+++ exception_handling-simple_calculator.py
@@ -2,31 +2,37 @@
 # Phoebe Rhone L. Gangoso | BSCPE 1-4
 
 def calculator_app():
-    # Ask user to choose and input math operation
-    input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
-    # Ask user to input two numbers
-    first_number = float(input("Enter your first number: "))
-    second_number = float(input("Enter your second number: "))
-    # Do the calculation
-    # if addition
-    if input_operation == "Addition":
-        result_number = first_number + second_number
-    # if subtraction
-    elif input_operation == "Subtraction":
-        result_number = first_number - second_number
-    # if multiplication
-    elif input_operation == "Multiplication":
-        result_number = first_number * second_number
-    # if division
-    else:
-        input_operation == "Division"
-        result_number = first_number / second_number
-    # display result
-    print(result_number)
-    # ask user if they want to try again or not
-    retry_input = input("Do you want to try again? (Yes/No): ")
-    # if yes repeat
-    if retry_input == "Yes":
-        calculator_app()
+    try:
+        # Ask user to choose and input math operation
+        input_operation = input("Choose one math operation (Addition, Subtraction, Multiplication, Division): ")
+        # Ask user to input two numbers
+        first_number = float(input("Enter your first number: "))
+        second_number = float(input("Enter your second number: "))
+        # Do the calculation
+        # if addition
+        if input_operation == "Addition":
+            result_number = first_number + second_number
+        # if subtraction
+        elif input_operation == "Subtraction":
+            result_number = first_number - second_number
+        # if multiplication
+        elif input_operation == "Multiplication":
+            result_number = first_number * second_number
+        # if division
+        elif input_operation == "Division":
+            result_number = first_number / second_number
+        else:
+            raise ValueError("Invalid math operation")
+        # display result
+        print(result_number)
+        # ask user if they want to try again or not
+        retry_input = input("Do you want to try again? (Yes/No): ")
+        # if yes repeat
+        if retry_input == "Yes":
+            calculator_app()
+
+    # catch exceptions
+    except ValueError as e:
+        print(f"Error: {e}. Please try again.")
 
 calculator_app()
